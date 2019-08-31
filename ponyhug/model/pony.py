67996d0ce4e30cf7007c22d5821029df7da3f7c7
marcsello <punkosdmarcell@rocketmail.com>
2019-08-31 02:34:30 +0200

    removed unique constraint on title

--- ponyhug/model/pony.py
+++ ponyhug/model/pony.py
@@ -6,6 +6,6 @@ class Pony(db.Model):
 
 	id = db.Column(db.Integer, primary_key=True, auto_increment=True)
 	key = db.Column(db.String(10), unique=True, nullable=False)
-	title = db.Column(db.String(255), unique=True, nullable=False)
+	title = db.Column(db.String(255), nullable=False)
 	source = db.Column(db.String(255), nullable=True)
 	image = db.Column(db.String(255), unique=True)
