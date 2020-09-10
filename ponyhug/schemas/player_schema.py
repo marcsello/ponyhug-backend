e5159d599bb9e413c284e928391d4e01a673f717
marcsello <punkosdmarcell@rocketmail.com>
2020-09-10 14:58:02 +0200

    Added playtime counter

--- ponyhug/schemas/player_schema.py
+++ ponyhug/schemas/player_schema.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from sqlalchemy import asc, desc
 from marshmallow import fields
 from marshmallow_sqlalchemy import ModelSchema
 from model import Player
@@ -8,9 +9,19 @@ from model import Hug
 
 class PlayerSchema(ModelSchema):
     hug_counter = fields.Method("get_hug_count", dump_only=True)
+    playtime = fields.Method("get_playtime", dump_only=True)
 
-    def get_hug_count(self, player):
+    def get_hug_count(self, player) -> int:
         return Hug.query.filter_by(player=player).count()
+
+    def get_playtime(self, player):
+        first_hug = Hug.query.filter_by(player=player).order_by(asc('timestamp')).first()
+        last_hug = Hug.query.filter_by(player=player).order_by(desc('timestamp')).first()
+
+        if not (first_hug and last_hug):
+            return None
+
+        return (last_hug.timestamp - first_hug.timestamp).total_seconds()
 
     class Meta:
         # Regular players won't be able to access this schema, so we can safely dump the is_admin
