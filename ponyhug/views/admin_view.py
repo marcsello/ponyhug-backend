2c61d8da3c9fd86647c1941e32a922701d611b73
marcsello <punkosdmarcell@rocketmail.com>
2020-09-10 14:03:49 +0200

    Added pony manipulation endpoints

--- ponyhug/views/admin_view.py
+++ ponyhug/views/admin_view.py
@@ -2,13 +2,18 @@
 from flask import abort, jsonify, current_app, request
 from flask_classful import FlaskView, route
 
-from utils import ponytoken_required, this_player, json_required, admin_required
+from marshmallow import ValidationError
+
+from utils import ponytoken_required, this_player, json_required, anyadmin_required, adminkey_required, admintoken_required
 from flask_jwt_simple import create_jwt
 
-from model import db, Player
+from model import db, Player, Pony
+from schemas import PonySchema
 
 
 class AdminView(FlaskView):
+    pony_schema = PonySchema(many=False)
+    ponies_schema = PonySchema(many=True)
 
     @ponytoken_required
     @json_required
@@ -32,20 +37,42 @@ class AdminView(FlaskView):
 
         return '', 204
 
-    @json_required
+    @anyadmin_required
     @route('/impersonate', methods=['POST'])
     def impersonate(self):
-
         params = request.get_json()
-        adminkey = params.get("key")
         playername = params.get("playername")
-
-        if adminkey != current_app.config['ADMIN_KEY']:
-            abort(401, "Invalid key")
 
         player = Player.query.filter(name=playername).first()
 
         if not player:
             abort(404, "No such player")
 
-        return {"jwt": create_jwt(identity=player.id), "playername": playername, "is_admin": False}, 200
+        return {"jwt": create_jwt(identity=player.id), "playername": playername, "is_admin": player.is_admin}, 200
+
+    @anyadmin_required
+    @json_required
+    @route('/ponies', methods=['POST'])
+    def createpony(self):
+        params = request.get_json()
+        try:
+            pony = self.pony_schema.load(params, session=db.session)
+        except ValidationError as e:
+            abort(400, str(e))
+
+        db.session.add(pony)
+        db.session.commit()
+        return jsonify(self.pony_schema.dump(pony)), 201
+
+    @anyadmin_required
+    @route('/ponies/<ponyid>', methods=['DELETE'])
+    def deletepony(self, ponyid: int):
+        Pony.query.filter_by(id=ponyid).delete()
+        db.session.commit()
+        return '', 204
+
+    @anyadmin_required
+    @route('/ponies', methods=['GET'])
+    def listponies(self):
+        ponies = Pony.query.all()
+        return jsonify(self.ponies_schema.dump(ponies)), 201
