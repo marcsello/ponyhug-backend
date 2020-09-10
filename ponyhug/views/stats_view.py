18445e0cdaa62220d1b1677e302acb7c45930e76
marcsello <punkosdmarcell@rocketmail.com>
2020-09-10 12:19:26 +0200

    Added stats view

---
+++ ponyhug/views/stats_view.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from sqlalchemy import func, desc
+from flask import jsonify
+from flask_classful import FlaskView
+
+from utils import ponytoken_required, admin_required
+
+from model import db, Hug
+
+
+class StatsView(FlaskView):
+
+    @admin_required
+    def index(self):
+        return 'TODO'
+
+    @ponytoken_required
+    def leader(self):
+        asd = db.session.query(
+            func.count(Hug.player_id).label('cnt'), Hug.player_id
+        ).group_by(Hug.player_id).order_by(desc('cnt')).first()
+
+        return jsonify({"hugs_of_leader": asd.cnt}), 200
