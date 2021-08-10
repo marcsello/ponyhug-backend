fe52768c5f15aaac41a7f193b40b03d23685b91b
marcsello <punkosdmarcell@rocketmail.com>
2021-08-10 23:53:22 +0200

    registration things

--- ponyhug/views/players_view.py
+++ ponyhug/views/players_view.py
@@ -35,6 +35,8 @@ class PlayersView(FlaskView):
     @timeframe_required
     def post(self):
 
+        # It seems like that validating, cleaning etc. isn't really solvable using marshmallow
+
         params = request.get_json()
         playername = params.get("name")
 
@@ -43,7 +45,7 @@ class PlayersView(FlaskView):
 
         # sanitize input
         playername_maxlen = Player.name.property.columns[0].type.length
-        playername = bleach.clean(playername, tags=[]).strip()[:playername_maxlen]  # cut to appropriate length
+        playername = bleach.clean(playername, tags=[], attributes={}).strip()[:playername_maxlen]  # cut to appropriate length
         # Length limiting is required here as SQLAlchemy does not validate the length of a field
         # If a database engine does not validate length (Like sqlite) that would lead to issues
 
