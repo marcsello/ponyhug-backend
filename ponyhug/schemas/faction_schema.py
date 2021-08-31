53b0074d74d5cd7db578412e95b1c64357b48102
marcsello <punkosdmarcell@rocketmail.com>
2021-08-31 23:53:42 +0200

    Removed broken hug counter from schema

--- ponyhug/schemas/faction_schema.py
+++ ponyhug/schemas/faction_schema.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python3
 from marshmallow import fields
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
-from model import Faction, Hug, Player
+from model import Faction, Player
 
 
 class FactionSchema(SQLAlchemyAutoSchema):
-    hug_count = fields.Method("get_hug_count", dump_only=True)
     member_count = fields.Method("get_member_count", dump_only=True)
-
-    def get_hug_count(self, faction) -> int:
-        return Hug.query.filter_by(faction=faction).count()
 
     def get_member_count(self, faction) -> int:
         return Player.query.filter_by(faction=faction).count()
