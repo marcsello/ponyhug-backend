bda5f06cffeb181cb9694dd02e2d096b47a831a8
marcsello <punkosdmarcell@rocketmail.com>
2021-09-05 01:51:42 +0200

    Added factions view

--- ponyhug/views/factions_view.py
+++ ponyhug/views/factions_view.py
@@ -11,4 +11,36 @@ from schemas import FactionSchema
 
 
 class FactionsView(FlaskView):
-    pass
+    faction_schema = FactionSchema(many=False)
+    factions_schema = FactionSchema(many=True)
+
+    decorators = [ponytoken_required]
+
+    def index(self):
+        factions = Faction.query.all()
+        return jsonify(self.factions_schema.dump(factions)), 200
+
+    def get(self, id_: int):
+        faction = Faction.query.filter_by(id=id_).first_or_404("No such faction")
+        return jsonify(self.faction_schema.dump(faction)), 200
+
+    def my(self):
+        return jsonify(self.faction_schema.dump(this_player().faction))
+
+    @anyadmin_required
+    @json_required
+    def post(self):
+        try:
+            faction = self.faction_schema.load(request.get_json(), session=db.session)
+        except ValidationError as e:
+            return abort(422, str(e))
+
+        db.session.add(faction)
+        db.session.commit()
+        return jsonify(self.faction_schema.dump(faction)), 201
+
+    @anyadmin_required
+    def delete(self, id_: int):
+        Faction.query.filter_by(id=id_).delete()
+        db.session.commit()
+        return '', 204
