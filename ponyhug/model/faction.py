f36d09493abe369888649ec59685282b8019b3b1
marcsello <punkosdmarcell@rocketmail.com>
2022-08-25 19:46:18 +0200

    Moved theme logic out of backend

--- ponyhug/model/faction.py
+++ ponyhug/model/faction.py
@@ -6,8 +6,6 @@ class Faction(db.Model):
     id = db.Column(db.Integer, primary_key=True, auto_increment=True)
     name = db.Column(db.String(50), unique=True, nullable=False)
 
-    accent = db.Column(db.String(7), nullable=False)
-    variant = db.Column(db.String(10), nullable=False)  # yes, bootstrap variant :/
-
-    icon_lg = db.Column(db.String(50), nullable=False)
-    icon_sm = db.Column(db.String(50), nullable=False)
+    # Must use values that are defined on the frontend
+    color_scheme = db.Column(db.String(15), nullable=False)
+    icon_set = db.Column(db.String(15), nullable=False)
