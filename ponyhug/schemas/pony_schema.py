fbfb465b33dc6609e72eb673c36ca368efac2f18
marcsello <punkosdmarcell@rocketmail.com>
2024-08-16 19:48:41 +0200

    better validation for pony creation

--- ponyhug/schemas/pony_schema.py
+++ ponyhug/schemas/pony_schema.py
@@ -3,7 +3,7 @@ from typing import Optional, Dict
 from marshmallow import fields
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 from sqlalchemy import asc
-from marshmallow.validate import Length, Regexp
+from marshmallow.validate import Length, Regexp, Range
 
 from model import Hug, Pony
 
@@ -12,6 +12,7 @@ class PonySchema(SQLAlchemyAutoSchema):
     first_hug = fields.Method("get_first_hug", dump_only=True)
     hugs = fields.Pluck('HugSchema', 'player', many=True, dump_only=True)
     key = fields.String(required=True, validate=[Length(equal=10), Regexp(r"^[0-9A-Z]*$")])
+    order = fields.Integer(required=True, validate=Range(min=1))
 
     def get_first_hug(self, pony) -> Optional[Dict[str, str]]:
         first_hug_for_this_pony = Hug.query.filter_by(pony=pony).order_by(asc('timestamp')).first()
@@ -26,6 +27,7 @@ class PonySchema(SQLAlchemyAutoSchema):
 
     class Meta:
         load_only = ['key']
+        dump_only = ['id']
         model = Pony
         include_relationships = True
         load_instance = True
